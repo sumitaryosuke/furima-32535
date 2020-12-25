e47f316d24ca4087551962fe150256a33cc45521
sumitaryosuke <f104193@gmail.com>
2020-12-25 09:33:10 +0900

    ユーザー管理機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,11 @@
 class ItemsController < ApplicationController
+<<<<<<< Updated upstream
+=======
+  def index
+  end
+
+  # def method_name
+
+  # end
+>>>>>>> Stashed changes
 end
---
+++ app/models/user.rb
@@ -0,0 +1,26 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :items
+  has_many :orders
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+
+  validates :nickname, presence: true
+  validates :birth_day, presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
+    validates :last_name, presence: true
+    validates :first_name, presence: true
+  end
+
+  with_options presence: true, format: { with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/, message: '全角カタカナを使用してください' } do
+    validates :last_name_kana, presence: true
+    validates :first_name_kana, presence: true
+  end
+
+end
