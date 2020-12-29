48455ce26eaee690c27b568a3676a347dd34bb70
sumitaryosuke <f104193@gmail.com>
2020-12-29 18:17:33 +0900

    コントローラー編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,23 @@ class ItemsController < ApplicationController
   end
 
   def new
-
+    @item = Item.new
   end
+
+  def create
+    # Prototype.create(prototype_params)
+    @item = Item.create(item_params)
+    if @item.save
+      redirect_to action: :index
+    else
+      render :new
+    end
+  end
+  private
+
+  def message_params
+    params.require(:item).permit(:image, :name, :price, :explanation, :area_id, :post_fee_id, :category_id, :post_day_id, :status_id).merge(user_id: current_user.id)
+  end
+
 end
+
