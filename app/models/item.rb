30b67adddcaf83d6c4eb58232fc3f05145a2098e
sumitaryosuke <f104193@gmail.com>
2020-12-29 18:17:18 +0900

    モデル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,21 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :user
+  belongs_to :area
+  belongs_to :category
+  belongs_to :post_day
+  belongs_to :status
+
+  validates :name,          presence: true
+  validates :explanation,   presence: true
+  validates :area_id,       numericality: { other_than: 1 }
+  validates :post_fee_id,   numericality: { other_than: 1 }
+  validates :category_id,   numericality: { other_than: 1 }
+  validates :post_day_id,   numericality: { other_than: 1 }
+  validates :status_id,     numericality: { other_than: 1 }
+
+  validates :price,         presence: true, format: { with: /\A[0-9]+\z/, message: '半角数字を使用してください' } do
+  end
+
+  has_one_attached :image
 end
