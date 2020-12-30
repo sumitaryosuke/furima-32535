9ec7a414c4968268ffb7e4376a5f27dcd726b445
sumitaryosuke <f104193@gmail.com>
2020-12-30 14:43:12 +0900

    バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,15 +7,17 @@ class Item < ApplicationRecord
   belongs_to :status
   belongs_to :post_fee
 
-  validates :name,          presence: true
-  validates :explanation,   presence: true
+  with_options presence: true do
+    validates :name
+    validates :explanation
+    validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  end
+
   validates :area_id,       numericality: { other_than: 0 }
   validates :post_fee_id,   numericality: { other_than: 1 }
   validates :category_id,   numericality: { other_than: 1 }
   validates :post_day_id,   numericality: { other_than: 1 }
   validates :status_id,     numericality: { other_than: 1 }
-  validates :price,         presence: true,
-                            numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
 
   has_one_attached :image
   validates :image, presence: true
