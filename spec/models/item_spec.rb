14e544c41a70056f44715a4207d62f6118ff007a
sumitaryosuke <f104193@gmail.com>
2020-12-30 14:42:58 +0900

    テストコード修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -47,9 +47,9 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include('Post fee must be other than 1')
       end
       it '商品の状態を選択しないと出品できない' do
-        @item.status_id = ''
+        @item.status_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include('Status is not a number')
+        expect(@item.errors.full_messages).to include('Status must be other than 1')
       end
       it '価格が300未満では出品できない' do
         @item.price = 299
@@ -70,6 +70,11 @@ RSpec.describe Item, type: :model do
         @item.user = nil
         @item.valid?
         expect(@item.errors.full_messages).to include('User must exist')
+      end
+      it '価格が数字以外では出品できない' do
+        @item.price = 'aaa'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is not a number')
       end
     end
   end
