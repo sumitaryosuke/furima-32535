4f5154da1b9d5a7814baa418758b902eeb270a77
sumitaryosuke <f104193@gmail.com>
2021-01-05 15:18:38 +0900

    redirect修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :redirect_user, only: [:edit, :destroy]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -23,7 +24,6 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    redirect_to action: :index unless current_user.id == @item.user_id
   end
 
   def update
@@ -39,7 +39,9 @@ class ItemsController < ApplicationController
   end
 
   private
-
+  def redirect_user
+    redirect_to action: :index unless current_user.id == @item.user_id
+  end
   def set_item
     @item = Item.find(params[:id])
   end
