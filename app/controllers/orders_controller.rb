9ce60ac621b29ec9664264b0e73a2e360eada87c
sumitaryosuke <f104193@gmail.com>
2021-01-08 15:12:23 +0900

    カード決済機能の実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -10,19 +10,15 @@ class OrdersController < ApplicationController
 
   def create
     # binding.pry
+    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
-    @order_address.save
-    # pay_item
+    # @order_address.save
+    pay_item
     if @order_address.valid?
       @order_address.save
       redirect_to items_path
-    begin
-      render :index
-    rescue => exception
-      
     else
-      
-    end
+      render action: :index
     end
   end
 
@@ -31,13 +27,14 @@ class OrdersController < ApplicationController
     params.require(:order_address).permit(:postal_code, :area_id, :city, :block, :building, :phone_number).merge( token: params[:token], item_id: params[:item_id], user_id: current_user.id)
   end
   
-  # def pay_item
-  #   Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
-  #   Payjp::Charge.create(
-  #     amount: order_params[:price],  # 商品の値段
-  #     card: order_params[:token],    # カードトークン
-  #     currency: 'jpy'                 # 通貨の種類（日本円）
-  #   )
-  # end
+  def pay_item
+    # item = Item.find(params[:item_id])
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: @item.price,  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
+  end
 
 end
